a2976049e98c1e6030ff3c48d8ef440eb4d73354
portLee <hungu32@gmail.com>
2024-02-05 17:48:00 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/challenge66/Challenge66Application.java
@@ -0,0 +1,13 @@
+package com.example.challenge66;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge66Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge66Application.class, args);
+	}
+
+}
